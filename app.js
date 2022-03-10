fcf5a3420a33509d6f42c8117352307fbcc2d1f3
Marlow Collins <marlowcollins95@gmail.com>
2022-03-10 00:12:05 -0600

    https added

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ var fileUploadRouter = require('./routes/fileUpload');
 
 var app = express();
 
+app.all('*',(req, res, next) => {
+  if(req.secure){
+    return next();
+  } else {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,9 +27,11 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(multer())
-app.use(cors())
+//app.use(cookieParser());
+//app.use(multer());
+//app.use(cors());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
