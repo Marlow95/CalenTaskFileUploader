6582ad046ae1c5da0247e188ef6645b2b638c1ea
Marlow Collins <marlowcollins95@gmail.com>
2022-03-10 01:14:20 -0600

    File uploader added

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var multer = require("multer");
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
@@ -27,9 +26,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(multer());
-//app.use(cors());
+app.use(cookieParser());
+app.use(cors());
 
 
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const multer = require('multer')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const fileUploadRouter = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname)
+    }
 });
 
-module.exports = router;
+const imageFileFilter = (req, file, cb) => {
+  if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+    return cb(new Error('You can only upload image files'), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage:
+storage, fileFilter: imageFileFilter });
+
+fileUploadRouter.route('/')
+
+.get((req, res, next) => {
+  res.statusCode = 403;
+  res.end('Operation not supported');
+})
+
+.post(upload.single('imageFile'),(req, res, next) => {
+  res.statusCode = 201;
+  res.setHeader('Content-Type','application/json');
+  res.json(req.file);
+})
+
+.put((req, res, next) => {
+  res.statusCode = 403;
+  res.end('Operation not supported');
+})
+
+.delete((req, res, next) => {
+  res.statusCode = 403;
+  res.end('Operation not supported');
+});
+
+module.exports = fileUploadRouter;
